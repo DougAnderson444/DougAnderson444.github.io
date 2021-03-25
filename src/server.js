ca81a28eb43668b407d52e67448a0d2b36142aec
Doug <douganderson444@gmail.com>
2021-03-24 20:41:46 -0400

    remove path

--- src/server.js
+++ src/server.js
@@ -5,13 +5,13 @@ import * as sapper from '@sapper/server'
 
 const { PORT, NODE_ENV } = process.env
 const dev = NODE_ENV === 'development'
-
+// const ghpage = !dev ? 'douganderson444.github.io' : ''
 polka() // You can also use Express
   .use(
-    'douganderson444.github.io',
     compression({ threshold: 0 }),
     sirv('static', { dev }),
     sapper.middleware()
+    // , ghpage
   )
   .listen(PORT, err => {
     if (err) console.log('error', err)
